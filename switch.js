cad8304b0ee2aab0c5d2c4a10fa537d3c65a085f
pranaytandel09 <pranaytandel09@gmail.com>
2023-02-07 09:48:41 +0530

    prob on switch case

---
+++ switch.js
@@ -0,0 +1,27 @@
+// prob on switch case.
+
+let day="Fri";
+switch(day){
+  case "Sun":
+    console.log("Sunday");
+    break;
+  case "Mon":
+    console.log("Monday");
+    break;
+  case "Tue":
+    console.log("Tuesday");
+    break;
+      case "Wed":
+    console.log("Wednesday");
+    break;
+  case "Thu":
+    consle.log("Thursday");
+    break;
+  case "Fri":
+    console.log("Friday");
+    break;
+  case "Sat":
+    console.log("Saturday");
+    break;
+    
+}
