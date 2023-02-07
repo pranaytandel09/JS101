89fec5a319f6973dcbb712072db83ae6b0b2a773
pranaytandel09 <pranaytandel09@gmail.com>
2023-02-07 09:39:33 +0530

    if else statement

---
+++ condition2.js
@@ -0,0 +1,13 @@
+// prob on if_else statement
+
+
+let item_prize=1000;
+let moneyIHave=800;
+if(moneyIHave>=item_prize)
+{
+  console.log("I bought the item");
+  
+}
+else{
+  console.log("Man I am broke");
+}
