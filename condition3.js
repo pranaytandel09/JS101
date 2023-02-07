ac351dbcd95ffa2250e2efaf8eeddf79c32dcdae
pranaytandel09 <pranaytandel09@gmail.com>
2023-02-07 09:42:30 +0530

    prob on if_else if_else statement

---
+++ condition3.js
@@ -0,0 +1,12 @@
+// prob on if_elseif_else;
+let a=100;
+let b=100;
+if(a>b){
+  console.log("a is greater than b");
+}
+else if(b>a){
+  console.log("b is greater than a");
+}
+else{
+  console.log("both are same");
+}
