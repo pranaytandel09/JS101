92d445741e28aeae4ea719af5f9ca149967817b6
pranaytandel09 <pranaytandel09@gmail.com>
2023-02-07 09:46:45 +0530

    logical operators

---
+++ logicalOperator.js
@@ -0,0 +1,8 @@
+// Enter no from 1 to 29
+let birthDate=29;
+if ((birthDate>=13) && (birthDate<=19)){
+  console.log("Teenage");
+}
+else if ((birthDate>=20) && (birthDate<=29)){
+  console.log("Twenties");
+}
